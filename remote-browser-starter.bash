94a3483302dab784c8357f73d38ba208a0059bc2
tomas-s9 <tomasbedac@gmail.com>
2024-09-09 11:55:17 -0300

    First setup

---
+++ remote-browser-starter.bash
@@ -0,0 +1,3 @@
+#conda activate stan
+#jupyter-lab --no-browser --port=8057 --NotebookApp.token='e295ehefewaphrfrepf0c55cf5e3b737f5b27fff4b1042ab57edxxx'
+jupyter-notebook --no-browser --port=8057 --NotebookApp.token='e295ehefewaphrfrepf0c55cf5e3b737f5b27fff4b1042ab57edxxx'
